23b3ba2b34014eafab22e53f1697bfbaefb8a759
dobby <tmakdlf2482@naver.com>
2024-08-31 21:20:25 +0900

    login

--- client/src/Component/User/Login.jsx
+++ client/src/Component/User/Login.jsx
@@ -1,9 +1,43 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { MDBContainer, MDBCol, MDBRow, MDBBtn, MDBInput } from 'mdb-react-ui-kit';
+import toast, { toastConfig } from 'react-simple-toasts';
+import 'react-simple-toasts/dist/theme/dark.css';
+import firebase from '../../firebase.js';
+import { useNavigate } from 'react-router-dom';
+
+toastConfig (
+  { theme: 'dark' }
+)
 
 function Login() {
   const [Email, setEmail] = useState('');
   const [PW, setPW] = useState('');
+  const [ErrorMsg, setErrorMsg] = useState('');
+
+  let navigate = useNavigate();
+
+  const signInFunc = async (e) => {
+    e.preventDefault();
+
+    if ( !(Email && PW) ) {
+      return toast('아이디 또는 비밀번호를 입력해주세요. 😓');
+    }
+
+    try {
+      await firebase.auth().signInWithEmailAndPassword(Email, PW);
+      // 로그인 성공 시
+      navigate('/');
+    }
+    catch(error) {
+      setErrorMsg('로그인에 실패하였습니다.');
+    }
+  };
+
+  useEffect(() => {
+    setTimeout(() => {
+      setErrorMsg('');
+    }, 5000);
+  }, [ErrorMsg]);
 
   return (
     <div className='container'>
@@ -18,7 +52,8 @@ function Login() {
               <MDBInput wrapperClass='mb-4' label='이메일' id='formControlLg' type='email' value={Email} size="md" className='shadow-none' onChange={(e) => setEmail(e.target.value)} />
               <MDBInput wrapperClass='mb-4' label='비밀번호' id='formControlLg' type='password' value={PW} size="md" className='shadow-none' onChange={(e) => setPW(e.target.value)} />
               <div className='text-center text-md-start mt-4 pt-2'>
-                <MDBBtn className="mb-0 px-5" size='md' color='dark' noRipple='true'>로그인</MDBBtn>
+                {ErrorMsg != '' && <p>{ErrorMsg}</p>}
+                <MDBBtn className="mb-0 px-5" size='md' color='dark' noRipple='true' onClick={(e) => signInFunc(e)}>로그인</MDBBtn>
                 <p className="small fw-bold mt-2 pt-1 mb-2">계정이 없으신가요? <a href="/register" className="link-danger" style={{ textDecorationLine: 'none' }}>회원가입</a></p>
               </div>
             </form>
