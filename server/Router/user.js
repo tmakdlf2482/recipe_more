a24d469581ba8c36a1870cbaa2fd2090d4484e67
dobby <tmakdlf2482@naver.com>
2024-09-01 07:26:34 +0900

    name overlap check(server)

--- server/Router/user.js
+++ server/Router/user.js
@@ -27,4 +27,23 @@ router.post('/register', (req, res) => {
   });
 });
 
+router.post('/nameCheck', (req, res) => {
+  User.findOne({ displayName: req.body.displayName }).exec()
+  .then((doc) => {
+    let check = true;
+    
+    if (doc) {
+      check = false; // 닉네임이 중복되면 false
+    }
+
+    // console.log(check);
+
+    res.status(200).json({ success: true , check });
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(400).json({ success: false });
+  });
+});
+
 module.exports = router;
